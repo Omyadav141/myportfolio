be992483131f1c49275486f019b92b082725e962
Om G. Yadav <omyadav9860@gmail.com>
2024-10-02 14:15:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const navbarMenu = document.querySelector(".navbar .links");
+        const menuBtn = document.querySelector(".navbar .menu-btn");
+        const hideMenuBtn = navbarMenu.querySelector(".close-btn");
+
+        //show mobile menu
+        menuBtn.addEventListener("click", () => {
+            navbarMenu.classList.toggle("show-menu");
+        });
+
+        //hide mobile menu
+        hideMenuBtn.addEventListener("click", () => menuBtn.click());
+
+
+
+// carousel
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
